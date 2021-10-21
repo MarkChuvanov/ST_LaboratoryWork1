1eab3fac803c3358be0a216693b32f64fa0fb3b6
MarkChuvanov <markchuvanov@mail.ru>
2021-10-21 11:10:15 +0300

    Init commit

---
+++ ST_LaboratoryWork1/ExaminationСommission.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ST_LaboratoryWork1
+{
+	public class Student
+	{
+		public string Name { get; set; }
+		public int? Mark { get; set; }
+		public bool IsAdmitted { get; set; }
+	}
+
+	public class CommissionMember
+	{
+		public string Name { get; set; }
+		public string Duty { get; set; }
+	}
+
+	public class ExaminationСommission
+	{
+		private List<Student> students;
+		public List<Student> Students
+		{
+			get
+			{
+				return students;
+			}
+		}
+
+		private List<CommissionMember> commission;
+		public List<CommissionMember> Commission
+		{
+			get
+			{
+				return commission;
+			}
+		}
+
+		private ExaminationСommission () { }
+
+		public ExaminationСommission (List<Student> students, List<CommissionMember> commission)
+		{
+			this.students = students;
+			this.commission = commission;
+		}
+
+		public int GetNumberOfAdmittedStudents ()
+		{
+			int count = 0;
+			foreach (Student student in students)
+			{
+				if (student.IsAdmitted)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public double GetAverageMarkOfExam ()
+		{
+			double amount = 0.0;
+			int count = 0;
+			foreach (Student student in students.Where(s => s.IsAdmitted == true))
+			{
+				count++;
+				amount += (int)student.Mark;
+			}
+			return amount / count;
+		}
+
+		public int GetNumberOfAcademicDebts ()
+		{
+			int count = 0;
+			foreach (Student student in students)
+			{
+				if (student.Mark == 2)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public void AddStudent (Student student)
+		{
+			if (student == null)
+			{
+				throw new System.NullReferenceException();
+			}
+			students.Add(student);
+		}
+
+		public void AddMemberOfCommision (CommissionMember member)
+		{
+			if (member == null)
+			{
+				throw new System.NullReferenceException();
+			}
+			commission.Add(member);
+		}
+	}
+}
---
+++ ST_LaboratoryWork1/ExaminationСommissionTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ST_LaboratoryWork1
+{
+	[TestFixture]
+	class ExaminationСommissionTest
+	{
+		private ExaminationСommission examinationCommission;
+
+		[SetUp]
+		public void Init ()
+		{
+			List<Student> students = new List<Student>()
+			{
+				new Student { Name = "Чуванов Марк Юрьевич", IsAdmitted = true, Mark = 5 },
+				new Student { Name = "Топкин Дмитрий Михайлович", IsAdmitted = true, Mark = 2 },
+				new Student{ Name = "Круглов Данила Александрович", IsAdmitted = false, Mark = null }
+			};
+			List<CommissionMember> commission = new List<CommissionMember>()
+			{
+				new CommissionMember { Name = "Привезенцев Денис Геннадьевич", Duty = "Доцент" },
+				new CommissionMember { Name = "Кульков Ярослав Юрьевич", Duty = "Доцент" }
+			};
+			examinationCommission = new ExaminationСommission(students, commission);
+		}
+
+		[Test]
+		public void GetNumberOfAdmittedStudentsTest ()
+		{
+			Assert.AreEqual(2, examinationCommission.GetNumberOfAdmittedStudents());
+			examinationCommission.AddStudent(new Student { Name = "Наумов Антон Владимирович", IsAdmitted = true, Mark = 4 });
+			Assert.AreEqual(3, examinationCommission.GetNumberOfAdmittedStudents());
+		}
+
+		[Test]
+		public void GetAverageMarkOfExamTest ()
+		{
+			Assert.AreEqual(3.5, examinationCommission.GetAverageMarkOfExam());
+			examinationCommission.AddStudent(new Student { Name = "Наумов Антон Владимирович", IsAdmitted = true, Mark = 2 });
+			Assert.AreEqual(3, examinationCommission.GetAverageMarkOfExam());
+		}
+
+		[Test]
+		public void GetNumberOfAcademicDebtsTest ()
+		{
+			Assert.AreEqual(1, examinationCommission.GetNumberOfAcademicDebts());
+			examinationCommission.AddStudent(new Student { Name = "Наумов Антон Владимирович", IsAdmitted = true, Mark = 2 });
+			Assert.AreEqual(2, examinationCommission.GetNumberOfAcademicDebts());
+		}
+
+		[Test]
+		public void AddStudentTest ()
+		{
+			examinationCommission.AddStudent(new Student { Name = "Наумов Антон Владимирович", IsAdmitted = true, Mark = 2 });
+			Assert.IsNotNull(examinationCommission.Students.Last());
+			Assert.Throws<NullReferenceException>(() => examinationCommission.AddStudent(null));
+			Assert.IsNotNull(examinationCommission.Students.Last());
+		}
+
+		[Test]
+		public void AddMemberOfCommisionTest ()
+		{
+			examinationCommission.AddMemberOfCommision(new CommissionMember { Name = "Быков Артем Александрович", Duty = "доцент" });
+			Assert.IsNotNull(examinationCommission.Commission.Last());
+			Assert.Throws<NullReferenceException>(() => examinationCommission.AddMemberOfCommision(null));
+			Assert.IsNull(examinationCommission.Commission.Last());
+		}
+	}
+}
